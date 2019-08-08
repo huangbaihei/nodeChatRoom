220875369f474fe762993a023bcf197ef330f408
黄麒二 <huangqier@hogecloud.com>
2019-08-08 15:22:46 +0800

    整理代码

--- js/room.js
+++ js/room.js
@@ -1,17 +1,25 @@
-// require('socket.io/socket.io.js');
-var userName, socket, tbxUsername, tbxMsg, divChat;
-function window_onload() {
+// io变量从socket.io/socket.io.js中获取，没有使用模块打包工具所以只能在html用script标签引入，不能用require
+var divChat, tbxUsername, tbxMsg, divRight, btnSend, btnLogout, btnLogin; // 节点变量
+var userName, socket;
+
+function window_onload() { // 页面加载完成后触发
   divChat = document.getElementById('divchat');
   tbxUsername = document.getElementById('tbxUsername');
   tbxMsg = document.getElementById('tbxMsg');
+  divRight = document.getElementById("divRight");
+  btnSend = document.getElementById("btnSend");
+  btnLogout = document.getElementById("btnLogout");
+  btnLogin = document.getElementById("btnLogin");
   tbxUsername.focus();
   tbxUsername.select();
 }
+
 function AddMsg(msg) {
   divChat.innerHTML += msg + '<br>';
   if (divChat.scrollHeight > divChat.clientHeight)
-    divChat.scrollTop = divChat.scrollHeight - divChat.clientHeight;
+    divChat.scrollTop = divChat.scrollHeight - divChat.clientHeight; // 让滚动条一直在底部
 }
+
 function btnLogin_onclick() {
   if (tbxUsername.value.trim() == '') {
     alert('请输入用户名');
@@ -21,11 +29,10 @@ function btnLogin_onclick() {
   socket = io.connect();
   socket.on('connect', function () {
     AddMsg("与聊天服务器的连接已建立。");
-    socket.on('login', function (name) {
+    socket.on('login', function (name) { // 监听登入
       AddMsg('欢迎用户' + name + '进入聊天室。');
     });
-    socket.on('sendClients', function (names) {
-      var divRight = document.getElementById("divRight");
+    socket.on('sendClients', function (names) { // 监听用户列表
       var str = "";
       names.forEach(function (name) {
         str += name + "<br/>"
@@ -33,59 +40,62 @@ function btnLogin_onclick() {
       divRight.innerHTML = "用户列表<br/>";
       divRight.innerHTML += str;
     });
-    socket.on('chat', function (data) {
+    socket.on('chat', function (data) { // 监听聊天消息
       AddMsg(data.user + '说:' + data.msg);
     });
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function () { // 监听连接断开
       AddMsg('与聊天服务器的连接已断开。');
-      document.getElementById("btnSend").disabled = true;
-      document.getElementById("btnLogout").disabled = true;
-      document.getElementById("btnLogin").disabled = "";
-      var divRight = document.getElementById("divRight");
+      btnSend.disabled = true;
+      btnLogout.disabled = true;
+      btnLogin.disabled = "";
       divRight.innerHTML = "用户列表";
     });
-    socket.on('logout', function (name) {
+    socket.on('logout', function (name) { // 监听登出
       AddMsg('用户' + name + '已退出聊天室。');
     });
-    socket.on('duplicate', function () {
+    socket.on('duplicate', function () { // 监听重复用户名
       alert('该用户名已被使用。');
-      document.getElementById("btnSend").disabled = true;
-      document.getElementById("btnLogout").disabled = true;
-      document.getElementById("btnLogin").disabled = "";
+      btnSend.disabled = true;
+      btnLogout.disabled = true;
+      btnLogin.disabled = "";
     });
   });
-  socket.on('error', function (err) {
+  socket.on('error', function (err) { // 监听连接错误
     AddMsg('与聊天服务器之间的连接发生错误。');
     socket.disconnect();
     socket.removeAllListeners('connect');
     io.sockets = {};
   });
   socket.emit('login', userName);
-  document.getElementById("btnSend").disabled = '';
-  document.getElementById("btnLogout").disabled = '';
-  document.getElementById("btnLogin").disabled = true;
+  btnSend.disabled = '';
+  btnLogout.disabled = '';
+  btnLogin.disabled = true;
 }
+
 function btnSend_onclick() {
   var msg = tbxMsg.value;
   if (msg.length > 0) {
-    socket.emit('chat', { user: userName, msg: msg });
+    socket.emit('chat', {
+      user: userName,
+      msg: msg
+    });
     tbxMsg.value = '';
   }
 }
+
 function btnLogout_onclick() {
   socket.emit('logout', userName);
   socket.disconnect();
-  socket.removeAllListeners('connect'
-  );
+  socket.removeAllListeners('connect');
   io.sockets = {};
   AddMsg("用户" + userName + "退出聊天室: ");
-  var divRight = document.getElementById("divRight");
   divRight.innerHTML = "用户列表";
-  document.getElementById("btnSend").disabled = "disabled";
-  document.getElementById("btnLogout").disabled = "disabled";
-  document.getElementById("btnLogin").disabled = "";
+  btnSend.disabled = "disabled";
+  btnLogout.disabled = "disabled";
+  btnLogin.disabled = "";
 }
-function window_onunload() {
+
+function window_onunload() { // 关闭窗口时触发
   socket.emit('logout', userName);
   socket.disconnect();
 }
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var express = require('express');
 var sio = require('socket.io');
 var app = express();
 var server = http.createServer(app);
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // 使用static中间件，可以引用到根目录下的所有静态文件
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/views/room.html');
+  res.sendFile(__dirname + '/views/room.html');
 });
 server.listen(80);
-console.log(123)
-var io = sio.listen(server);
-var names = [];
-io.sockets.on('connection', function (socket) {
-  socket.on('login', function (name) {
+console.log('服务器已启动...');
+var io = sio.listen(server); // 使用socket.io维持http连接
+var names = []; // 存放客户端连接的数组
+io.sockets.on('connection', function (socket) {  // 使用io.sockets可以实现消息广播
+  socket.on('login', function (name) {  // 监听客户端登入
     for (var i = 0; i < names.length; i++) {
       if (names[i] == name) {
         socket.emit('duplicate');
@@ -23,17 +23,17 @@ io.sockets.on('connection', function (socket) {
     io.sockets.emit('login', name);
     io.sockets.emit('sendClients', names);
   });
-  socket.on('chat', function (data) {
+  socket.on('chat', function (data) {  // 监听客户端聊天消息
     io.sockets.emit('chat', data);
   });
-  socket.on('logout', function (name) {
+  socket.on('logout', function (name) { // 监听客户端登出
     for (var i = 0; i < names.length; i++) {
       if (names[i] == name) {
         names.splice(i, 1);
         break;
       }
     }
-    socket.broadcast.emit('logout', name);
+    socket.broadcast.emit('logout', name); // socket.broadcast也可以实现消息广播
     io.sockets.emit('sendClients', names);
   });
 });
